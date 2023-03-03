f38c2b6506c5d3d034266ed264ef6473712b1dc0
Pavel <Test@mail.ru>
2023-03-03 07:56:18 +0400

    editing the code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,10 @@
 public class CreditPaymentService {
-    public int calculate(double loanAmount, double loanTerm, double interestRate){
+    public int calculate(double loanAmount, double loanTerm, double interestRate) {
+
         double interestRateMec = interestRate / 12 / 100;
-        double koff = (interestRateMec * Math.pow(1 + interestRateMec, loanTerm)) / (Math.pow(1 + interestRateMec, loanTerm) - 1);
-        double payment = koff * loanAmount;
-        //double payment = loanAmount * interestRateMec * Math.pow(1 + interestRateMec, loanTerm) / (Math.pow(1 + interestRateMec, loanTerm) - 1);
-        return(int) payment;
-        }
+        double ratio = (interestRateMec * Math.pow(1 + interestRateMec, loanTerm)) / (Math.pow(1 + interestRateMec, loanTerm) - 1);
+        double payment = ratio * loanAmount;
+
+        return (int) payment;
     }
+}
